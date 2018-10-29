82ec6b3fb3116cbd4f7b5ee98cb51197aeb2247f
burnyd <burnyd@gmail.com>
2018-10-29 11:12:41 -0400

    Added comments

--- cvpsalt.py
+++ cvpsalt.py
@@ -6,6 +6,8 @@ import urllib3
 
 urllib3.disable_warnings()
 
+
+"""Checks to see if the cvp.sls file is currently within the pillars"""
 def config():
     config = __salt__['config.get']('cvp')
     if not config:
@@ -14,6 +16,7 @@ def config():
         )
     return config
 
+"""Check to see if the pillar file has all the necessary informaiton"""
 def config_dict():
     cvp_dict = {}
     cvp_dict['server'] = config().get('server')
@@ -21,15 +24,19 @@ def config_dict():
     cvp_dict['password'] = config().get('password')
     return cvp_dict
 
+"""Connects to the cvp API"""
 def connect_cvp():
     urllib3.disable_warnings()
     client = CvpClient()
     client.connect([config_dict()['server']], config_dict()['username'], config_dict()['password'])
     return client
 
+"""salt 'minionid' cvptest.load_config 'ONUG' 'hostname test-device' would simply add text config to the cvp server"""
 def load_config(config_name, configlet):
     client = connect_cvp()
     add = client.api.add_configlet(config_name, configlet)
+
+"""salt 'minionid' cvptest.load_template 'salt-test' salt://logging2.cfg would simply render a template from a specific location to the cvp master."""
 
 def load_template(config_name, configlet):
     client = connect_cvp()
